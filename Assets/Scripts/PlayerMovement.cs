12b80ec70abf79ccda526427f1abdf589912f246
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-05 09:23:45 -0500

    Fixed Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D RB2D;
 
     public float speed;
+    public float JumpForce;
+
     private Rigidbody2D rb;
     void Start()
     {
-<<<<<<< Updated upstream
+
         rb = GetComponent<Rigidbody2D>();
-        speed = 5f;
+        //speed = 5f;
     }
+    
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxisRaw("Horizontal") * speed;
@@ -21,18 +23,16 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxisRaw("Vertical") * speed;
 
         rb.velocity = new Vector2(moveHorizontal, moveVertical);
-=======
-        RB2D = GetComponent<Rigidbody2D>();
     }
+    /*
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
         RB2D.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
->>>>>>> Stashed changes
     }
-
+    */
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.gameObject.name);

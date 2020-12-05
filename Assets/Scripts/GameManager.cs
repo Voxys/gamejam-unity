30611ffb05d481399c37cc3fc548f6c96dcba042
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-05 10:31:38 -0500

    GameManager update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager Instance;
+
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
     }
+
+
 }

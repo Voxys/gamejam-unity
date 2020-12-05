ad95dec046ac53155bfa1023a378c36cea57be2a
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-05 15:35:43 -0500

    Menu fonctionnel 🔨
    Play button OK

---
+++ Assets/Scripts/Menu/ButtonSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSelector : MonoBehaviour
+{
+	public Button PlayBtn;
+
+	void Start()
+	{
+		Button btn = PlayBtn.GetComponent<Button>();
+		btn.onClick.AddListener(Play);
+	}
+
+	void Play()
+	{
+		SceneManager.LoadScene("Assets/Scenes/WorldMap.unity");
+	}
+}

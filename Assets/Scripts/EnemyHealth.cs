ff73744aa8349b1311aa6fd267776a4a2f52cbaf
Romain Noel <1931809@cgmatane.qc.ca>
2020-12-05 09:28:06 -0500

    add player spell 1 and 2

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class EnnemyHealth : MonoBehaviour
+public class EnemyHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int maxHealth = 100;
+    public int currentHealth;
+    public int damage = 15;
+
+    public HealthBar healthBar;
+
+    private void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F) && currentHealth > 0)
+        {
+            TakeDamage(damage);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
--- Assets/Scripts/PlayerSpells.cs
+++ Assets/Scripts/PlayerSpells.cs
@@ -1,13 +1,61 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerSpells : MonoBehaviour
 {
     public int healing = 15;
+    public Transform target;
+    public float Speed = 4;
+    private Animator animator;
+    private Vector3 initialePosition;
+    private bool isFlipped = false;
 
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        initialePosition = transform.position;
+    }
 
     public void Normal_Attack()
     {
-    
+        StartCoroutine(Cr_Attack());
+    }
+
+    public void Update()
+    {
+
+    }
+
+    IEnumerator Cr_Attack()
+    {
+        
+        while (Mathf.Abs(transform.position.x - target.transform.position.x) > 2.5f)
+        {
+            animator.SetBool("Run", true);
+            transform.Translate(Speed * Time.deltaTime, 0, 0);
+            yield return new WaitForEndOfFrame();
+            animator.SetBool("Run", false);
+        }
+
+        animator.SetTrigger("Attack");
+
+        yield return new WaitForSeconds(0.5f);
+        Flip();
+        while (Mathf.Abs(transform.position.x - initialePosition.x) > 0.2f)
+        {
+            animator.SetBool("Run", true);
+            transform.Translate(-Speed * Time.deltaTime, 0, 0);
+            yield return new WaitForEndOfFrame();
+            animator.SetBool("Run", false);
+        }
+        Flip();
+
+    }
+
+    private void Flip()
+    {
+        isFlipped = !isFlipped;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
 
@@ -15,4 +63,5 @@ public class PlayerSpells : MonoBehaviour
     {
         PlayerHealth.instance.HealPlayer(healing);
     }
+
 }

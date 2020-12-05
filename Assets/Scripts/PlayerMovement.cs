3d7c10e1328f9fac908147d8a1e732770fabdb40
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-04 20:50:51 -0500

    Update PlayerMovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-
-    private Rigidbody2D rb2d;
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+
     }
     void FixedUpdate()
     {
@@ -19,6 +17,6 @@ public class PlayerMovement : MonoBehaviour
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-        rb2d.AddForce(movement * speed);
+        transform.Translate(movement * speed);
     }
 }

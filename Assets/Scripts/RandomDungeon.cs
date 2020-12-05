a565077b678b7735daf0fa57a7aea05cd25c9fa6
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-04 22:37:53 -0500

    Dungeon Randomizer 🔨

--- Assets/Scripts/RandomDungeon.cs
+++ Assets/Scripts/RandomDungeon.cs
@@ -10,12 +10,12 @@ public class RandomDungeon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int t_RandomGrid = Random.Range(10, 20);
-        grid.RowCount = t_RandomGrid;
-        grid.ColumnCount = t_RandomGrid;
+        //int t_RandomGrid = Random.Range(10, 20);
+        //grid.RowCount = t_RandomGrid;
+        //grid.ColumnCount = t_RandomGrid;
         RandomFill();
     }
-
+    
     private void RandomFill()
     {
         for (int i=1; i<grid.RowCount - 1;i++)
@@ -32,7 +32,8 @@ public class RandomDungeon : MonoBehaviour
                 Instantiate(t_tile, t_pos, Quaternion.identity);
             }
         }
-
+        /*
+        
         for (int i=0; i<1;i++)
         {
             for (int j=0;j<grid.ColumnCount;j++)
@@ -56,16 +57,19 @@ public class RandomDungeon : MonoBehaviour
         {
             InstantiateWall(grid.ColumnCount-1, i);
         }
+        */
     }
-
+    
     private void InstantiateWall(int i, int j)
     {
-        Sprite t_Sprite = Wall.GetComponent<SpriteRenderer>().sprite;
+        GameObject t_tile = tiles[2];
+        Sprite t_Sprite = t_tile.GetComponent<SpriteRenderer>().sprite;
         float t_Scale = 1 / t_Sprite.bounds.size.x;
-        Wall.transform.localScale = new Vector3(t_Scale, t_Scale, t_Scale);
+        t_tile.transform.localScale = new Vector3(t_Scale, t_Scale, t_Scale);
         //Vector3 t_pos = new Vector3(i, j, 0);
         Vector3 t_pos = new Vector3(i + 0.5f, j + 0.5f, 0);
-        Instantiate(Wall, t_pos, Quaternion.identity);
+        Instantiate(t_tile, t_pos, Quaternion.identity);
     }
+    
 
 }

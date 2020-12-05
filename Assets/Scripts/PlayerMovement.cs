47ea9773abc51daa1fd50fc0a4d5f07bf046c530
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-05 09:11:34 -0500

    movement update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D RB2D;
+
     public float speed;
     private Rigidbody2D rb;
     void Start()
     {
+<<<<<<< Updated upstream
         rb = GetComponent<Rigidbody2D>();
         speed = 5f;
     }
@@ -18,6 +21,16 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxisRaw("Vertical") * speed;
 
         rb.velocity = new Vector2(moveHorizontal, moveVertical);
+=======
+        RB2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        RB2D.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+>>>>>>> Stashed changes
     }
 
     private void OnCollisionEnter2D(Collision2D collision)

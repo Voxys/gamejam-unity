423f2ad50d2ef9d8add4b1f4391f9cd4a8a6cf90
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-05 16:07:14 -0500

    Dungeon Sound Added

--- Assets/Scripts/DungeonMonsterManager.cs
+++ Assets/Scripts/DungeonMonsterManager.cs
@@ -8,12 +8,12 @@ public class DungeonMonsterManager : MonoBehaviour
     public float stoppingDistance;
     public float seeingDistance;
 
-
+    private AudioSource Sound;
     private Transform target;
 
     void Start()
     {
-        
+        Sound = GetComponent<AudioSource>();
         target = GameObject.FindWithTag("Player").GetComponent<Transform>();
     }
 
@@ -21,7 +21,10 @@ public class DungeonMonsterManager : MonoBehaviour
     {
         if(Vector2.Distance(transform.position,target.position)>stoppingDistance && Vector2.Distance(transform.position, target.position) < seeingDistance)
         {
+            Sound.Play();
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
+
+        Sound.Stop();
     }
 }

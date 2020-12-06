b4c549d2e6c7381231883bdb1bb5de8cad00b4bb
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-06 16:11:07 -0500

    Pathfinding low budget ✔

--- Assets/Scripts/Pathfinding/Spawner.cs
+++ Assets/Scripts/Pathfinding/Spawner.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject Villager;
+    public bool CanSpawn;
+    public float SpawnInterval=8f;
+
+    private float m_LastSpawnTime;
 
     void Start()
     {
-        Instantiate(Villager);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Time.time - m_LastSpawnTime >= SpawnInterval)
+        {
+            m_LastSpawnTime = Time.time;
+            Instantiate(Villager);
+        }
+
     }
 }

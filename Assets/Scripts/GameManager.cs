1c6774000f8048b6deb07c2e52064e92f428cc8a
Voxys <50014072+Voxys@users.noreply.github.com>
2020-12-05 23:10:04 -0500

    ➕ GameManager Ajout

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     
     public GameObject WorldPlayer;
+    public Scene SceneActive;
 
     public static int NumberOfVisitedRoom;
 

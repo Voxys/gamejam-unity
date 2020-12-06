c11492657aaf5d781cb50e03745ef283f150dcd4
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-06 09:36:34 -0500

    EndMenu🔨

---
+++ Assets/Scripts/EndMenu/ButtonSelectorEndMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonSelectorEndMenu : MonoBehaviour
+{
+	public Button RetryBtn;
+
+	void Start()
+	{
+		Button btnRetry = RetryBtn.GetComponent<Button>();
+
+		btnRetry.onClick.AddListener(Retry);
+	}
+
+	void Retry()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+}

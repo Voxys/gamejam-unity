0f7408446c5c6f4ef5826124a12a993a7e2c7c2b
Voxys <50014072+Voxys@users.noreply.github.com>
2020-12-05 19:01:54 -0500

    Dialogue en cours 🔨

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public GameObject BulleDialogue;
+    public GameObject CanvasDialogue;
+    public GameObject Dialogue1;
+    public GameObject Dialogue2;
+    public bool CollideWithPnj;
+    public int compteurDialogue;
+
+    void Start()
+    {
+        BulleDialogue.SetActive(false);
+        CanvasDialogue.SetActive(false);
+        Dialogue1.SetActive(false);
+        Dialogue2.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && CollideWithPnj)
+        {
+            CanvasDialogue.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CollideWithPnj = true;
+            BulleDialogue.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+            CollideWithPnj = false;
+            BulleDialogue.SetActive(false);
+            CanvasDialogue.SetActive(false);
+    }
+
+    public void GestionDialogue()
+    {
+        compteurDialogue++;
+
+        if (compteurDialogue == 1)
+        {
+            Dialogue1.SetActive(true);
+        }
+
+        if(compteurDialogue == 2)
+        {
+            Dialogue1.SetActive(false);
+            Dialogue2.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/WorldMap/EnterHouses.cs
+++ Assets/Scripts/WorldMap/EnterHouses.cs
@@ -21,9 +21,51 @@ public class EnterHouses : MonoBehaviour
             SceneManager.LoadScene("House1", LoadSceneMode.Additive);
         }
 
+        if (collision.gameObject.CompareTag("House2_Entrance"))
+        {
+            player.enabled = false;
+            SceneManager.LoadScene("House2", LoadSceneMode.Additive);
+        }
+
+        if (collision.gameObject.CompareTag("Shop_Entrance"))
+        {
+            player.enabled = false;
+            SceneManager.LoadScene("Shop", LoadSceneMode.Additive);
+        }
+
+        if (collision.gameObject.CompareTag("DungeonLobby_Entrance"))
+        {
+            player.enabled = false;
+            SceneManager.LoadScene("DungeonLobby", LoadSceneMode.Additive);
+        }
+
+        if (collision.gameObject.CompareTag("Dungeon_Entrance"))
+        {
+            player.enabled = false;
+            SceneManager.LoadScene("Dungeon");
+        }
+
         if (collision.gameObject.CompareTag("House1_Exit"))
         {
             SceneManager.UnloadSceneAsync("House1");
+            GameManager.Instance.WorldPlayer.GetComponent<PlayerMovement>().enabled = true;
+        }
+
+        if (collision.gameObject.CompareTag("House2_Exit"))
+        {
+            SceneManager.UnloadSceneAsync("House2");
+            GameManager.Instance.WorldPlayer.GetComponent<PlayerMovement>().enabled = true;
+        }
+
+        if (collision.gameObject.CompareTag("Shop_Exit"))
+        {
+            SceneManager.UnloadSceneAsync("Shop");
+            GameManager.Instance.WorldPlayer.GetComponent<PlayerMovement>().enabled = true;
+        }
+
+        if (collision.gameObject.CompareTag("DungeonLobby_Exit"))
+        {
+            SceneManager.UnloadSceneAsync("DungeonLobby");
             GameManager.Instance.WorldPlayer.GetComponent<PlayerMovement>().enabled = true;
         }
     }

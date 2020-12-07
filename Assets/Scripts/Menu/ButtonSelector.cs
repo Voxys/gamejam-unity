9fa1180b9d66a7160d90bcbd0bcb306320d871f4
poiuyt1996 <poiuyt1996@hotmail.com>
2020-12-06 19:36:04 -0500

    Menu update

--- Assets/Scripts/Menu/ButtonSelector.cs
+++ Assets/Scripts/Menu/ButtonSelector.cs
@@ -7,16 +7,24 @@ using UnityEngine.SceneManagement;
 public class ButtonSelector : MonoBehaviour
 {
 	public Button PlayBtn;
+	public Button StoryBtn;
 
 	void Start()
 	{
 		Button btnPlay = PlayBtn.GetComponent<Button>();
+		Button btnStory = StoryBtn.GetComponent<Button>();
 
 		btnPlay.onClick.AddListener(Play);
+		btnStory.onClick.AddListener(Story);
 	}
 
 	void Play()
 	{
 		SceneManager.LoadScene("StartScene");
 	}
+
+	void Story()
+    {
+		Debug.Log("story");
+    }
 }
